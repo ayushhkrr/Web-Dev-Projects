39793a03e653c16562ce089aa3ebb1f33ebf33b1
ayushhkrr <ayushhkrr@gmail.com>
2025-09-04 13:59:47 +0530

    Update index.js

--- Loops Practise/index.js
+++ Loops Practise/index.js
@@ -1,25 +1,5 @@
-// for (let i = 5; i >= 1; i--) {
-//     let pattern = '';
-//     for (let j = 1; j <= i; j++) {
-//         pattern += '&';
-//     }
-//     console.log(pattern);
-// }
-// factor = 1
-// for (i = 1; i <= 5; i++){
-//     factor = factor * i;
 
-// }
-// console.log(factor)
 
-// for (i = 1; i < 6; i++){
-//     let pattern = "*";
-//     for (k = 1; k < i; k++){
-//         pattern += "*";
-//     }
-//         console.log(pattern)
-
-// }
 
 // let num = 57
 // let isprime = true
@@ -49,7 +29,7 @@
 //     x1 = x2
 //     x2 = xx
 // }
-  
+
 // for (i = 1; i < 5; i++){
 //   let pattern = "*"
 //   for (x = 1; x < i; x++){
@@ -59,14 +39,89 @@
 // console.log(pattern)
 // }
 
-for (i = 5; i > 0; i--){
-  let pattern = ""
-  for (j = 0; j < i; j++){
-    pattern += "&"
-  }
-  console.log(pattern)
+// for (i = 5; i > 0; i--){
+//   let pattern = ""
+//   for (j = 0; j < i; j++){
+//     pattern += "&"
+//   }
+//   console.log(pattern)
+// }
+
+// let multiply = (a, b)=> {
+//   return a*b
+// }
+
+// let car = {
+//   brand: "BMW",
+//   color: "Black",
+//   year: 1969
+
+// }
+
+// console.log(car.brand);
+// console.log(car["color"]);
+
+// let person = {
+//   name: "Ayush",
+//   age: 20,
+//   city: "Delhi",
+// };
+
+// person.isStudentTrue = true;
+// person.age = 21;
+// delete person.city;
+
+// console.log(person);
+
+// let new_arr = ["one", "two", "three"]
+
+// let one = "first"
+// let two = "second"
+// let three = "third"
+
+// console.log(Array.of(one, two, three));
+
+const obj = {
+  name: "Ayush",
+  Age: 20,
+  Religion: "Hindu",
+  warning : function () {
+ return "You Are In Danger";
 }
 
-let multiply = (a, b)=> {
-  return a*b
+};
+
+
+// console.log(obj);
+
+// console.log({
+//   ...obj,
+//   warning: obj.warning(),
+// });
+
+// obj.newWarning = function () {
+//   console.log(`You Are In Danger ${this.name}`);
+  
+// }
+
+// console.log(obj.newWarning());
+
+const first = {
+  name: "Roshan",
+  age: 21
 }
+const second = {
+  word: "Ayush",
+  number: 20
+}
+
+// const third = Object.assign({}, first, second)
+
+// console.log(third);
+
+// console.log(Object.keys(first));
+// console.log(Object.values(first));
+// console.log(Object.entries(first));
+console.log(first.hasOwnProperty("name"));
+
+
