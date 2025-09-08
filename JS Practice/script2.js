4fff227c6218456051ca06b4488d77dfe00a21b0
ayushhkrr <ayushhkrr@gmail.com>
2025-09-08 12:56:34 +0530

    Update script2.js

--- JS Practice/script2.js
+++ JS Practice/script2.js
@@ -1,6 +1,7 @@
 // let a = 64
 // let b = 69
 
+
 // a = a + 5
 // b = b - 5
 
@@ -37,23 +38,53 @@
     
 // }
 
-let citizens = {
-    Abhinav: {
-        Age: 22
-    },
-    Roshan: {
-        Age: 21
-    },
-    Nikil: {
-        Age: 12
+// let citizens = {
+//     Abhinav: {
+//         Age: 22
+//     },
+//     Roshan: {
+//         Age: 21
+//     },
+//     Nikil: {
+//         Age: 12
+//     }
+// }
+
+// for (let name in citizens) {
+// if (citizens[name].Age >= 18) {
+//   console.log(` ${name} is eligible to vote.`);
+// } else {
+//     console.log(`${name} is not eligible for vote.`);
+    
+// }
+// }
+
+// const fun =  () => {
+
+//     if (true) {
+//         let a = 10;
+//         const b = 20;
+//         var c = 30;
+//         console.log(a, b, c);
+//         return [a, b, c];
+//     }
+// }
+
+// console.log(...fun());
+
+for (i = 1; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 ===0){
+    console.log('FizzBuzz');
+    
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+        
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+        
+    } else {
+        console.log(i);
+        
     }
 }
 
-for (let name in citizens) {
-if (citizens[name].Age >= 18) {
-  console.log(` ${name} is eligible to vote.`);
-} else {
-    console.log(`${name} is not eligible for vote.`);
-    
-}
-}
